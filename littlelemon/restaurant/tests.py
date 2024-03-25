adad0c19a90e24227473d3c6ee59928469451023
EnCo-de <142403662+EnCo-de@users.noreply.github.com>
2024-03-25 22:09:24 +0400

    add tests before API

--- littlelemon/restaurant/tests.py
+++ littlelemon/restaurant/tests.py
@@ -1,3 +1,21 @@
 from django.test import TestCase
+from restaurant.models import Booking
+import datetime
 
 # Create your tests here.
+class BookingTestCase(TestCase):
+    def setUp(self):
+        Booking.objects.create(first_name='John', reservation_date='2024-03-20')
+        Booking.objects.create(first_name='Jane', reservation_date='2024-03-24')
+
+    def test_booking_date(self):
+        """The booking by John should be on 2024-03-20."""
+        booking_date = datetime.date(2024, 3, 20)
+        customer = Booking.objects.filter(first_name='John')
+        self.assertEqual(customer.first().reservation_date, booking_date, 'fail message')
+
+    def test_booking_count(self):
+        """Two bookings by John and Jane should be added."""
+        # self.createBooking()
+        customers = Booking.objects.all()
+        self.assertEqual(customers.count(), 2, 'fail message')
