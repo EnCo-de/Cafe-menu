67149dbb944c3084e8d08a2eb888ec1946ff94eb
EnCo-de <142403662+EnCo-de@users.noreply.github.com>
2024-03-28 23:12:23 +0400

    add items to cart 1

--- littlelemon/LittleLemonAPI/views.py
+++ littlelemon/LittleLemonAPI/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from django.http import JsonResponse, Http404
+from .models import MenuItem
 
 # Create your views here.
+def add_menu_item(request, pk):
+    try:
+        item = MenuItem.objects.get(pk=pk)
+        data = {'dish': item.name, 'price': item.price}
+    except MenuItem.DoesNotExist:
+        return JsonResponse({'data':'wrong url'})
+    return JsonResponse(data)
--- littlelemon/littlelemon/urls.py
+++ littlelemon/littlelemon/urls.py
@@ -17,7 +17,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
+from LittleLemonAPI.views import add_menu_item
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('restaurant.urls')),
+    path('api/<int:pk>', add_menu_item),
 ]
